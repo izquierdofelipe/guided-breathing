a5d310893a216f874f5eb0814c58ef39e380b2e1
Felipe Izquierdo <felipedizquierdo@gmail.com>
2025-06-09 09:05:39 -0400

    fix: update HTTP to HTTPS redirection logic to only trigger when behind a proxy

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,12 @@ import path from 'path';
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Redirect HTTP to HTTPS
+// Redirect HTTP to HTTPS only when behind a proxy (Heroku)
 app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
+  if (
+    req.headers['x-forwarded-proto'] &&
+    req.headers['x-forwarded-proto'] !== 'https'
+  ) {
     return res.redirect(`https://${req.hostname}${req.url}`);
   }
   next();
